617e2afd519af5bea26551d840d068d1ab9edf92
Imran Vohra <Imran Vohra@DESKTOP-3B9VMNI>
2022-02-26 02:28:46 +0530

    Multiplying two numbers

---
+++ Aspire2022/src/aspireLabBook2/Multiplication.java
@@ -0,0 +1,17 @@
+package aspireLabBook2;
+
+import java.util.Scanner;
+public class Multiplication {
+
+	public static void main(String[] args) {
+		System.out.println("Let us multiply two numbers.");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the first number: ");
+		int first = scan.nextInt();
+		System.out.print("Enter the second number: ");
+		int second = scan.nextInt();
+		int product = first*second;
+		System.out.println("The product of the numbers is: " + product + ".");
+	}
+
+}
